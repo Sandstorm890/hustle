ff11a4690ac8992ebd1df677a47ac9955c88862f
zaynegoering <zaynegoering@gmail.com>
2021-01-17 13:59:57 -0800

    nest jobs under employers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
-  resources :employers
-  resources :job
+  resources :employers do
+    resources :jobs
+  end
 end
