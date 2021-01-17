0e227084464204031c3438d25213b14a2988dc70
zaynegoering <zaynegoering@gmail.com>
2021-01-17 14:14:27 -0800

    add basic routes to controllers

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,19 @@
 class JobsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
