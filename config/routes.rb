8dc146f0169dd427422ccd62feb95fa25221524a
zaynegoering <zaynegoering@gmail.com>
2021-01-18 11:23:59 -0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'sessions#welcome'
+
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   resources :users
   resources :employers do
     resources :jobs
