a86ad5591948936c0e77e10c1ede88aceac036cf
zaynegoering <zaynegoering@gmail.com>
2021-01-19 14:16:32 -0800

    fill out multiple methods

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,10 @@
 class JobsController < ApplicationController
     def index
+        @jobs = Job.all
     end
 
     def new
+        @job = Job.find_by(id: 1)
     end
 
     def create
@@ -12,8 +14,21 @@ class JobsController < ApplicationController
     end
 
     def update
+        @job = Job.find(params[:id])
+        @job.update(jobs_params)
+        if @job.valid?
+            redirect_to jobs_path 
+        else
+            render :edit 
+        end
     end
 
     def destroy
+    end
+
+    private
+
+    def jobs_params
+        params.require(:job).permit(:category, :start_date, :end_date, :rate, :requirements, :user_id, :employer_id)
     end
 end
