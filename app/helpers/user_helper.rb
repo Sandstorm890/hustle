7b7aed0c465c2b8031ec54b58f1a38da631ba7e5
zaynegoering <zaynegoering@gmail.com>
2021-01-19 15:03:30 -0800

    move helpers to app helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,22 +1,5 @@
 module UserHelper
 
-    def show_logged_in
-        if session[:user_id]
-            content_tag(:h2, "Hello #{current_user.name}")
-        end
-    end
-
-    def current_user
-        @user = User.find_by(id: session[:user_id])
-    end
-
-    def current_user_is_employer?
-        # byebug
-        if current_user == nil
-            false
-        else
-            current_user.employer
-        end
-    end
+    
 
 end
