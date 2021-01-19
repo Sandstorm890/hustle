27f56a4a82c623b26658f3130b4c3e84207a983c
zaynegoering <zaynegoering@gmail.com>
2021-01-19 15:02:42 -0800

    fill out methods

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -4,17 +4,39 @@ class EmployersController < ApplicationController
     end
 
     def new
+        @employer = Employer.new
     end
 
     def create
+        @employer = Employer.new(employer_params)
+        byebug
+        if @employer.save
+            session[:employer_id] = @employer.id
+            byebug
+            redirect_to employer_path(@employer)
+        else
+            render :new
+        end
     end
 
     def show
+        @employer = Employer.find(params[:id])
     end
 
     def update
     end
 
+    def logout
+        session.clear[:Employer_id]
+        redirect :index
+    end
+
     def destroy
+    end
+
+    private
+
+    def employer_params
+        params.require(:employer).permit(:name, :username, :email, :password, :bio)
     end
 end
