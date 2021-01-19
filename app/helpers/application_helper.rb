20247b9433c1fe8a2d57e436fff9a81748408d5b
zaynegoering <zaynegoering@gmail.com>
2021-01-19 15:03:18 -0800

    move heleprs back

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+    def show_logged_in
+        if session[:employer_id] || session[:user_id]
+            content_tag(:h2, "Hello #{current_user.name}")
+        end
+    end
+
+    def current_user
+        if session[:employer_id]
+            @user = Employer.find_by(id: session[:employer_id])
+        else
+            @user = User.find_by(id: session[:user_id])
+        end
+    end
+
+    def employer?
+        !!session[:employer_id]
+    end
+
 end
