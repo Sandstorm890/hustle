22b35597e7813a4f3a30bccbbee7932474ad2a69
zaynegoering <zaynegoering@gmail.com>
2021-01-20 23:45:47 -0800

    add worker? employer? helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 
     def show_logged_in
-        if session[:employer_id] || session[:user_id]
+        if logged_in?
             content_tag(:h2, "Hello #{current_user.name}")
         end
     end
 
     def current_user
         if session[:employer_id]
-            @user = Employer.find_by(id: session[:employer_id])
+            @employer = Employer.find_by(id: session[:employer_id])
         else
             @user = User.find_by(id: session[:user_id])
         end
@@ -16,6 +16,14 @@ module ApplicationHelper
 
     def employer?
         !!session[:employer_id]
+    end
+
+    def worker?
+        !!session[:user_id]
+    end 
+
+    def logged_in?
+        worker? || employer?
     end
 
 end
