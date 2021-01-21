580ea8c3e6fcce8f17c5a52e9a11eacfdd845598
zaynegoering <zaynegoering@gmail.com>
2021-01-20 23:49:38 -0800

    add job_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_222651) do
+ActiveRecord::Schema.define(version: 2021_01_21_011455) do
 
   create_table "employers", force: :cascade do |t|
     t.string "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_222651) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "job_id"
   end
 
 end
