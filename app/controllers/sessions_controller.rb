13d875221a5e05cf2a7b27511d704bbb877d9a42
zaynegoering <zaynegoering@gmail.com>
2021-01-20 23:45:07 -0800

    make login process dynamic for users/employers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
     def welcome
-        # byebug
         if session[:user_id]
             redirect_to '/users'
         else
@@ -15,14 +14,22 @@ class SessionsController < ApplicationController
 
     def destroy 
         session.delete(:user_id)
+        session.delete(:employer_id)
         redirect_to '/'
     end
 
     def create 
-        user = User.find_by_email(params[:user][:email])
-        if user && user.authenticate(params[:user][:password])
-            session[:user_id] = user.id
-            redirect_to user_path(user)
+        if user = User.find_by(email: params[:email])
+            
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect_to user_path(user)
+            end
+        elsif employer = Employer.find_by_email(params[:email])
+            if employer && employer.authenticate(params[:password])
+                session[:employer_id] = employer.id
+                redirect_to employer_path(employer)
+            end
         else
             flash[:message] = "Invalid login. Please try again."
             redirect_to '/login'
