3e258c9a4e60a3d160d029415afc41fd5a823a6e
zaynegoering <zaynegoering@gmail.com>
2021-01-20 23:44:11 -0800

    fill out methods

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,13 +4,42 @@ class JobsController < ApplicationController
     end
 
     def new
-        @job = Job.find_by(id: 1)
+        @job = Job.new
+        @employer = Employer.find_by(id: session[:employer_id])
     end
 
     def create
+        @job = Job.new(jobs_params)
+        @job.employer_id = session[:employer_id]
+        if @job.save
+            redirect_to job_path(@job)
+        else
+            render :show
+        end
+    end
+
+    def sign_up_for_job
+        job = Job.find_by(id: params[:job_id])
+        job.users << current_user
+        job.save
+        redirect_to job_path(job)
+    end
+
+    def remove_from_job
+        job = Job.find_by(id: params[:job_id])
+        if job.users.include?(current_user)
+            job.users.each do |user|
+                if user == current_user
+                    job.users.delete(current_user)
+                end
+            end
+            job.save
+        end
+        redirect_to job_path(job)
     end
 
     def show
+        @job = Job.find_by(id: params[:id])
     end
 
     def update
@@ -30,5 +59,13 @@ class JobsController < ApplicationController
 
     def jobs_params
         params.require(:job).permit(:category, :start_date, :end_date, :rate, :requirements, :user_id, :employer_id)
+    end
+
+    def current_user
+        if session[:employer_id]
+            @employer = Employer.find_by(id: session[:employer_id])
+        else
+            @user = User.find_by(id: session[:user_id])
+        end
     end
 end
