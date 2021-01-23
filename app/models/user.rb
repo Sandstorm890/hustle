9b41b5d49a88feb5faf7478840c6d60165a1dbbe
zaynegoering <zaynegoering@gmail.com>
2021-01-23 15:52:33 -0800

    change association and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     has_and_belongs_to_many :jobs
     has_many :employers, through: :jobs
-    has_many :tools
+    has_and_belongs_to_many :tools
     has_many :skills
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true
-    validates :password, length: {in: 6..20}
+    validates :password, length: {in: 3..20}
     has_secure_password
 end
