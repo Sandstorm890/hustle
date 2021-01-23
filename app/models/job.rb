6013b0c8a2de40f1d1d1a6ad4011049424181483
zaynegoering <zaynegoering@gmail.com>
2021-01-23 15:51:57 -0800

    add validations for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,14 @@
 class Job < ApplicationRecord
     has_and_belongs_to_many :users
     belongs_to :employer
+
+    validates :address, presence: true
+    validates :category, presence: true
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    validates :rate, presence: true
+
+    def self.order_by_rate
+        order(rate: :desc)
+    end
 end
