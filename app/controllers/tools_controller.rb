adb6686a523431679d9f393faddbb85546e090e0
zaynegoering <zaynegoering@gmail.com>
2021-01-22 19:26:33 -0800

    add tools controller

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,22 @@
+class ToolsController < ApplicationController
+
+    def index
+        @tools = Tool.all
+    end
+
+    def update
+        tool = current_tool
+        if tool.users.include?(current_user)
+            tool.users.delete(current_user)
+            current_user.tools.delete(current_tool)
+        else
+            tool.users << current_user
+            current_user.tools << tool
+            tool.save
+            current_user.save
+        end
+        redirect_to tools_path
+    end
+
+
+end
