a4ee72e513b3f68a47493e6f089c35504ba8167d
zaynegoering <zaynegoering@gmail.com>
2021-01-22 19:25:48 -0800

    move helpers to here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,19 @@ class ApplicationController < ActionController::Base
 
     def index
     end
+
+    def current_user
+        if session[:employer_id]
+            @employer = Employer.find_by(id: session[:employer_id])
+        else
+            @user = User.find_by(id: session[:user_id])
+        end
+    end
+
+    def current_tool
+        @tool = Tool.find_by(id: params[:id])
+    end
+
+
     
 end
