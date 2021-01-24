c9f79d3517c36c8abc2b0a1989e63e1570a1d5b6
zaynegoering <zaynegoering@gmail.com>
2021-01-24 11:44:25 -0800

    add method for OAuth2 and remove name validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,20 @@ class User < ApplicationRecord
     has_many :employers, through: :jobs
     has_and_belongs_to_many :tools
     has_many :skills
-    validates :name, presence: true
+    # validates :first_name, presence: true
+    # validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true
-    validates :password, length: {in: 3..20}
+    # validates :password_digest, presence: true
     has_secure_password
+
+    def self.from_omniauth(response)
+        User.find_or_create_by(uid: response[:uid]) do |u|
+            u.first_name = response[:info][:first_name]
+            u.last_name = response[:info][:last_name]
+            u.username = response[:info][:name]
+            u.email = response[:info][:email]
+            u.password = SecureRandom.hex(15)
+        end
+    end
 end
